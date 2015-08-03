3cd3ca0b31e91ed04b2c657d6bdeb19885cbff6e
Lauren Weiss <laurenw258@lpsk12.org>
2015-08-03 14:20:44 -0700

    Revert d884041..018a4c2
    This rolls back to commit d884041385dd42cd64ddfafd6b7b2af8a3ce600f.

--- LevelData.swift
+++ LevelData.swift
@@ -49,7 +49,6 @@ struct Data {
     var counters = [String: Int]()
     
     var MyNewCareerScientistImage: String!
-    var unlocked:Bool = false
     
     
 }
@@ -103,7 +102,6 @@ struct LevelData {
         level1.spawnThese = [3,1,2,3]
         level1.randomMin = 1
         level1.randomMax = 3
-//        level1.passed = false
         
         
         //add intern career level
